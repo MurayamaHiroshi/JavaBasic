4305b5f244b75b42be5118f499453ca60aad3d20
Murayama Hiroshi <mhkick3@gmail.com>
2018-05-16 18:25:42 +0900

    １６＿１

--- Practice/src/practice16/PTra16_01.java
+++ Practice/src/practice16/PTra16_01.java
@@ -1,12 +1,13 @@
 package practice16;
-
+import java.util.Date;
 /*
  */
 public class PTra16_01 {
     public static void main(String[] args) {
-
         /*
         * ★ 現在時刻を「YYYY年MM月DD日」形式で出力してください
         */
+    	Date date = new Date();
+    	System.out.println(date);
     }
 }
