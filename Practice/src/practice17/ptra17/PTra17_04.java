1f2fe64a7887b27c0f75dcf5b7431d52318937f7
Murayama Hiroshi <mhkick3@gmail.com>
2018-05-24 15:37:45 +0900

    １７＿４　

--- Practice/src/practice17/ptra17/PTra17_04.java
+++ Practice/src/practice17/ptra17/PTra17_04.java
@@ -6,6 +6,8 @@
  */
 package practice17.ptra17;
 
+import practice09.util.Util;
+
 public class PTra17_04 {
 	public static void main(String[] args) {
 
@@ -15,9 +17,8 @@ public class PTra17_04 {
 		 * ★ strに入っている文字列を1文字ずつゆっくり表示させるプログラムを完成させてください
 		 * ※ Thread.sleep を使用すること
 		 */
-
+		Util.dispMessageSlowly(str,500);
 
 	}
 }
-？？？
 //前回行ってたpracticeの同じプログラムをコピペしてやればできる。
